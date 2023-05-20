859c46311f1c773f655b41cd7f0a9cc69dadb19b
David Yappeter <davidyap11les@gmail.com>
2023-05-20 21:40:13 +0700

    feat: add Temporary AuthProvider

---
+++ src/core/Auth/AuthProvider.jsx
@@ -0,0 +1,104 @@
+import axios from 'axios'
+
+const { createContext, useContext, useState } = require('react')
+
+const AUTH_KEY = process.env.REACT_APP_AUTH_LOCAL_STORAGE_KEY || 'auth-token'
+
+const getAuthToken = () => {
+  if (!localStorage) {
+    return
+  }
+
+  const authToken = localStorage.getItem(AUTH_KEY)
+  if (!authToken) {
+    return
+  }
+
+  try {
+    const authParsed = JSON.parse(authToken)
+    if (authParsed) {
+      return authParsed
+    }
+  } catch (error) {
+    console.log('PARSE ERROR', error)
+  }
+}
+
+const setAuthToken = (auth) => {
+  if (!localStorage) {
+    return
+  }
+
+  try {
+    const authToken = JSON.stringify(auth)
+    localStorage.setItem(AUTH_KEY, authToken)
+  } catch (error) {
+    console.error('SAVE ERROR', error)
+  }
+}
+
+const removeAuthToken = () => {
+  if (!localStorage) {
+    return
+  }
+
+  try {
+    localStorage.removeItem(AUTH_KEY)
+  } catch (error) {
+    console.error('REMOVE ERROR', error)
+  }
+}
+
+const initialAuthContext = {
+  token: getAuthToken(),
+  setToken: (token) => null,
+  currentUser: undefined,
+  refetchAuthToken: () => null,
+  logout: () => null,
+}
+
+const AuthContext = createContext(initialAuthContext)
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+  const [token, setTokenState] = useState(getAuthToken())
+
+  axios
+    .post('https://money-be.mikroskil.com/users/me')
+    .then((r) => r.data.data?.user)
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  const setToken = (newToken) => {
+    if (newToken) {
+      setAuthToken(newToken)
+      setTokenState(newToken)
+    } else {
+      removeAuthToken()
+    }
+  }
+
+  const logout = () => {
+    setToken(undefined)
+    window.location.reload()
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        setToken,
+        currentUser,
+        logout,
+        refetchAuth: refetch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
