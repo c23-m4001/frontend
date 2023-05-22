4167c0856990f94e8cfc21c1000b03dfb3227cf8
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-05-22 11:38:12 +0700

    feat: add dashboard page

---
+++ src/page/dashboard/Dashboard.jsx
@@ -0,0 +1,27 @@
+import { useIntl } from 'react-intl'
+
+import { useModal } from '../../core/Modal/ModalProvider'
+import { useAuth } from '../../core/Auth/AuthProvider'
+
+export const DashboardPage = () => {
+  const { setModal, toggleModal } = useModal()
+  const intl = useIntl()
+  const { currentUser } = useAuth()
+
+  return (
+    <div>
+      <h1>{intl.formatMessage({ id: 'TEST.QWE' })}</h1>
+      <div>test {JSON.stringify(currentUser)}</div>
+      <button
+        type="button"
+        onClick={() => {
+          setModal(<div>Hello World Hello WorldHello WorldHello World</div>)
+          toggleModal()
+        }}
+        className="btn btn-primary"
+      >
+        Primary
+      </button>
+    </div>
+  )
+}
