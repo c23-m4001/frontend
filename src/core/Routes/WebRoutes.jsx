dbd5d269abf06b1e8154fd21695f849dad57a9f4
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-05-22 11:39:35 +0700

    feat: add auth page path

--- src/core/Routes/WebRoutes.jsx
+++ src/core/Routes/WebRoutes.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthPage } from '../../page/auth/AuthPage'
+import { useAuth } from '../Auth/AuthProvider'
+import { DashboardPage } from '../../page/dashboard/Dashboard'
 
 export const WebRoutes = () => {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="auth/*"
-          element={<AuthPage />}
-        />
+        {currentUser ? (
+          <Route path="/*" element={<DashboardPage />} />
+        ) : (
+          <Route path="auth/*" element={<AuthPage />}
+          />
+        )}
       </Routes>
     </BrowserRouter>
   )
--- src/page/auth/AuthPage.jsx
+++ src/page/auth/AuthPage.jsx
@@ -1,9 +1,17 @@
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { Loginpage } from './login/LoginPage'
 
 export const AuthPage = () => {
   return (
     <Routes>
-      
+      <Route
+        path="/register"
+        element={<Loginpage />}
+      />
+      <Route
+        path="/login"
+        element={<Loginpage />}
+      />
     </Routes>
   )
 }
