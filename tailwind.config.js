5b07fc8a64a74d3c130bcc217b12183b28646f9c
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-05-23 11:46:43 +0700

    feat: add custom color & width

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,45 @@ module.exports = {
         'modal-fade': 'modal-fade 0.2s linear forwards',
       },
       colors: {
-        primary: '#3DA9FC',
+        'primary': '#3DA9FC',
         'primary-hover': '#3DD9DC',
         'primary-inverse': '#FFFFFF',
 
-        secondary: '#90B4CE',
+        'secondary': '#90B4CE',
 
-        white: '#FFFFFF',
-        backdrop: 'rgb(13,12,12,0.6)',
+        'white': '#FFFFFF',
+        'backdrop': 'rgb(13,12,12,0.6)',
+        'background': '#D8EEFE',
+        'headline': '#094067',
+        'paragraph': '#5F6C7B',
+        'danger': '#FF0000',
+        'success': '#3DA9FC',
       },
       borderRadius: {
         '10px': '10px',
+      },
+      borderColor: {
+        secondary: '#90B4CE',
       },
       fontFamily: {
         poppins: ['Poppins'],
       },
       maxWidth: {
         '800px': '800px',
+      },
+      minHeight: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+      },
+      minWidth: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
       },
       spacing: {
         '1px': '1px',
