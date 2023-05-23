98d6bfb7741b8ab7540bc194ca47320b238f2a7b
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-05-23 08:31:09 +0700

    feat: add register method

--- src/page/auth/register/RegisterInput.jsx
+++ src/page/auth/register/RegisterInput.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useInput } from "../../../custom-hooks/useInput";
+import { AuthApi } from '../../../api/auth/authApi';
+import { useAuth } from "../../../core/Auth/AuthProvider";
 
 export const RegisterInput = ({ register }) => {
   const [name, onNameChange] = useInput('');
   const [email, onEmailChange] = useInput('');
   const [password, onPasswordChange] = useInput('');
+  const [domainErrors, setDomainErrors] = useState({});
+  const { currentUser,setToken } = useAuth();
+
+  console.log("CURRE", currentUser)
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    
+    const a = await AuthApi.emailRegister({ name, email, password }).
+    catch((er) => {
+      setDomainErrors(er.response.data.errors.reduce((prev, err) => {
+        prev[err.domain] = err.message
+        return prev
+      }, {}))
+    })
+
+    setToken(a.data.token.access_token)
+  }
 
   return (
-    <form className="form-register">
+    <form className="form-register" onSubmit={onSubmit}>
       <input placeholder="name" className="inputAuth" name="text" type="text" value={name} onChange={onNameChange} />
+      {domainErrors?.name && <div>
+        error: {domainErrors?.name}</div>}
       <input placeholder="email" className="inputAuth" name="text" type="email" value={email} onChange={onEmailChange} />
+      {domainErrors?.email && <div>
+        error: {domainErrors?.email}</div>}
       <input placeholder="password" className="inputAuth" name="text" type="password" value={password} onChange={onPasswordChange} />
-      <button type="button" onClick={() => register({ email, password })}>Register</button>
+      {domainErrors?.password && <div>
+        error: {domainErrors?.password}</div>}
+      <button type="submit">Register</button>
     </form>
   )
 }
