47e886caafbced2a18351c528a26e5bb3e4c1e19
David Yappeter <davidyap11les@gmail.com>
2023-05-25 13:20:47 +0700

    refactor: Button and ProfileModal component

--- src/components/modals/ProfileModal.jsx
+++ src/components/modals/ProfileModal.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useAuth } from '../../core/Auth/AuthProvider'
 import { Button } from '../button/Button'
 import { BiHistory, BiLogOutCircle } from 'react-icons/bi'
 
 export const ProfileModal = ({ isOpen }) => {
   const { currentUser, logout } = useAuth()
+
   return (
     <div
       className={`${
         isOpen ? 'active' : 'hidden'
-      } select-none rounded-10px absolute mt-60 md:mt-72 flex-col bg-white p-3 md:p-6 max-w-180px md:max-w-sm md:max-h-42 drop-shadow-md`}
+      } fixed top-80px right-20px select-none rounded-10px flex-col bg-white p-3 md:p-6 max-w-180px md:max-w-sm md:max-h-42 drop-shadow-md`}
     >
       <div>
         <div className="flex gap-2 md:gap-4 mb-4 text-sm md:text-base">
@@ -20,28 +22,33 @@ export const ProfileModal = ({ isOpen }) => {
               !open && 'transform rotate-180'
             }`}
           />
-          <div className='truncate'>
+          <div className="truncate">
             <p>{currentUser.name}</p>
             <p>{currentUser.email}</p>
           </div>
         </div>
       </div>
-      <div className='border-t-2 p-2 text-sm'>
-        <div className="flex items-center text-secondary gap-2 md:gap-4">
+      <div className="border-t-2 p-2 text-sm">
+        <Link
+          to={'/login-histories'}
+          type="button"
+          className="flex items-center text-secondary gap-2 md:gap-4"
+          // onClick={navigate('/login-histories')}
+        >
           <BiHistory className="flex justify-center text-2xl" />
           <p>Login History</p>
-        </div>
+        </Link>
       </div>
-      <div className='p-2 text-sm'>
-        <div className="flex items-center text-secondary gap-2 md:gap-4">
+      <div className="p-2 text-sm">
+        <Button
+          type="button"
+          label="Logout"
+          onClick={() => logout()}
+          className="flex items-center text-secondary gap-2 md:gap-4"
+        >
           <BiLogOutCircle className="flex justify-center transform rotate-180 text-2xl" />
-          <Button
-            type={'button'}
-            btnName={'Logout'}
-            className={''}
-            onClick={() => logout()}
-          />
-        </div>
+          <p>Logout</p>
+        </Button>
       </div>
     </div>
   )
