acedaee104d78e6241526cf74722f38f0e648680
David Yappeter <davidyap11les@gmail.com>
2023-05-30 14:37:42 +0700

    feat: add moment and implementation

--- src/page/login_history/LoginHistoryItem.jsx
+++ src/page/login_history/LoginHistoryItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import LoginHistoryItemBody from './LoginHistoryItemBody'
+import moment from 'moment/moment'
 
 function LoginHistoryItem({ time, location_name, ip_address }) {
   return (
     <div className="block max-w-600px my-5 mx-auto p-6 bg-white border border-gray-200 rounded-xl shadow">
       <LoginHistoryItemBody
-        time={time}
+        time={moment(time).format('MMMM Do YYYY, h:mm:ss a')}
         location_name={location_name}
         ip_address={ip_address}
       />
