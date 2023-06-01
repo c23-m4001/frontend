2856e7b04a509d2a78fabc6ff16542af0f11646f
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-06-01 15:22:09 +0700

    fix: container to be the same as wallet page

--- src/page/transaction/Transaction.jsx
+++ src/page/transaction/Transaction.jsx
@@ -66,25 +66,28 @@ export const TransactionPage = ({ amount }) => {
   const onTransactionClick = (id) => {
     const transactionNow = nodes.filter((transaction) => transaction.id === id)
     setModal(
-      <div className="flex gap-2">
-        <div className="flex flex-2 gap-2 items-center">
-          <img
-            src="/svgs/avatar.svg"
-            className="w-6"
-          />
-          <div className="flex flex-col gap-2 text-xs">
-            <p className="text-headline text-base font-bold">
+      <div className="flex flex-col sm:flex-row sm:gap-4 md:py-6">
+        <div className="flex flex-2 sm:flex-row-reverse gap-6 pb-4">
+          <div className="flex flex-col text-xs">
+            <p className="text-headline text-lg font-bold md:text-2xl">
               {transactionNow[0].name}
             </p>
-            <p className="text-paragraph">
+            <p className="text-paragraph pb-2 md:text-base">
               {moment(transactionNow.date).format('ddd, MMM Do YYYY')}
             </p>
-            <p className="bg-primary rounded-lg text-white w-max px-4">
+            <p className="bg-primary rounded-lg text-white text-sm w-max px-4 md:text-base">
               {transactionNow[0].category.name}
             </p>
           </div>
+          <img
+            src="/svgs/avatar.svg"
+            className="w-8 md:w-12"
+          />
         </div>
-        <p className="flex flex-1 justify-end items-center text-right text-green-600 text-sm">
+        <p
+          className="flex flex-1 justify-end items-center text-right text-green-600 font-semibold
+            text-lg py-2 border-t border-secondary sm:border-0 sm:py-0 sm:pb-4 md:text-2xl"
+        >
           Rp. {transactionNow[0].amount}
         </p>
       </div>
@@ -95,18 +98,18 @@ export const TransactionPage = ({ amount }) => {
   const onDeleteHandler = (id) => {
     const transactionNow = nodes.filter((transaction) => transaction.id === id)
     setModal(
-      <div className="flex flex-col justify-center p-4 text-center gap-4">
+      <div className="flex flex-col justify-center py-4 text-center gap-4">
         <p>Anda yakin ingin menghapus transaksi?</p>
-        <div className='flex justify-center gap-4'>
+        <div className="flex justify-center gap-4">
           <Button
             type={'button'}
-            className='btn bg-danger text-white rounded-full'
+            className="btn bg-danger text-white rounded-full"
           >
             Hapus
           </Button>
           <Button
             type={'button'}
-            className='btn bg-white border border-paragraph text-paragraph rounded-full'
+            className="btn bg-white border border-paragraph text-paragraph rounded-full"
           >
             Batal
           </Button>
@@ -115,89 +118,79 @@ export const TransactionPage = ({ amount }) => {
     )
     showModal()
   }
-
   return (
-    <div className="relative flex flex-col items-center min-h-screen bg-background text-white">
-      {isLoading ? (
-        <h1>Loading</h1>
-      ) : (
-        <>
-          <div className="bg-primary w-full drop-shadow-lg flex justify-center text-lg sm:text-xl md:text-2xl">
-            <div className="w-4/5 md:max-w-800px flex flex-col">
-              <h1 className="my-8 top-0">Transaksi</h1>
-              <div className="flex flex-col items-center">
-                <h2 className="pb-4 font-semibold">Cashflow</h2>
-                <div className="font-semibold text-3xl sm:text-4xl pb-28">
-                  Rp.{amount}4,750,000
-                </div>
+    <div className="min-h-screen bg-background">
+      <div className="w-full flex flex-col space-y-6 py-40px px-20px sm:px-100px lg:px-200px items-center">
+        <div className="w-full flex flex-col items-start text-headline text-lg sm:text-xl md:text-2xl">
+          <h1 className="font-bold text-2xl">Transaction</h1>
+          <div className="w-full flex flex-col items-center py-6">
+            <h2 className="pb-4 font-semibold">Cashflow</h2>
+            <div className="font-semibold text-3xl sm:text-4xl">
+              Rp.{amount}4,750,000
+            </div>
+          </div>
+        </div>
+        <div className="w-full sm:w-4/5 md:max-w-800px sm:gap-8 flex flex-col sm:flex-row items-center">
+          <div className="w-4/5 flex flex-col items-center bg-white text-green-600 p-8 rounded-md mb-4">
+            <div className="font-semibold text-xl pb-1">
+              Rp.{amount}4,750,000
+            </div>
+            <p className="font-bold text-headline">Gross Income</p>
+          </div>
+          <div className="w-4/5 flex flex-col items-center bg-white text-danger p-8 rounded-md mb-4">
+            <div className="font-semibold text-xl pb-1">Rp.{amount}200,000</div>
+            <p className="font-bold text-headline">Expense</p>
+          </div>
+        </div>
+        <div className="w-full sm:w-4/5 md:max-w-800px flex flex-col items-center mt-60 sm:mt-24">
+          <div className="w-4/5 sm:w-full flex flex-col items-center bg-white text-headline p-2 rounded-md mb-4">
+            <div className="w-full sm:px-8 md:px-16 flex text-center text-xs sm:text-sm md:text-base lg:text-lg py-3 border-b-2">
+              <div className="flex-1">Last month</div>
+              <div className="flex-1">This month</div>
+              <div className="flex-1">Future</div>
+            </div>
+            <div className="w-full flex flex-col gap-2 sm:px-12 md:px-16 text-xs sm:text-sm md:text-base p-3">
+              <div className="flex justify-between">
+                <p>Starting balance</p>
+                <p>Rp. 4,750,000</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Inflow</p>
+                <p className="text-green-600">+Rp. 680,000</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Outflow</p>
+                <p className="text-danger">-Rp. 2,000,000</p>
+              </div>
+            </div>
+            <div className="w-full px-3 sm:px-12 md:px-16 text-xs sm:text-sm md:text-base pb-3">
+              <div className="flex justify-between pt-3 border-t-2">
+                <p>Total</p>
+                <p>Rp. 3,430,000</p>
               </div>
             </div>
           </div>
 
-          <div className="w-full sm:w-4/5 md:max-w-800px sm:gap-8 absolute flex flex-col sm:flex-row items-center mt-60">
-            <div className="w-4/5 flex flex-col items-center bg-white text-green-600 p-8 rounded-md mb-4">
-              <div className="font-semibold text-xl pb-1">
-                Rp.{amount}4,750,000
-              </div>
-              <p className="font-bold text-headline">Gross Income</p>
+          <div className="w-4/5 sm:w-full flex flex-col items-center bg-white text-headline p-2 rounded-md mb-4">
+            <div className="w-full flex text-center text-xs sm:text-sm md:text-base lg:text-lg py-3 border-b-2">
+              <div className="flex-1">This month</div>
             </div>
-            <div className="w-4/5 flex flex-col items-center bg-white text-danger p-8 rounded-md mb-4">
-              <div className="font-semibold text-xl pb-1">
-                Rp.{amount}200,000
-              </div>
-              <p className="font-bold text-headline">Expense</p>
-            </div>
-          </div>
-          <div className="w-full sm:w-4/5 md:max-w-800px flex flex-col items-center mt-60 sm:mt-24">
-            <div className="w-4/5 sm:w-full flex flex-col items-center bg-white text-headline p-2 rounded-md mb-4">
-              <div className="w-full sm:px-12 md:px-16 flex text-center text-xs sm:text-sm md:text-base lg:text-lg py-3 border-b-2">
-                <div className="flex-1">Last month</div>
-                <div className="flex-1">This month</div>
-                <div className="flex-1">Future</div>
-              </div>
-              <div className="w-full flex flex-col gap-2 sm:px-12 md:px-16 text-xs sm:text-sm md:text-base p-3">
-                <div className="flex justify-between">
-                  <p>Starting balance</p>
-                  <p>Rp. 4,750,000</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Inflow</p>
-                  <p className="text-green-600">+Rp. 680,000</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Outflow</p>
-                  <p className="text-danger">-Rp. 2,000,000</p>
-                </div>
-              </div>
-              <div className="w-full px-3 sm:px-12 md:px-16 text-xs sm:text-sm md:text-base pb-3">
-                <div className="flex justify-between pt-3 border-t-2">
-                  <p>Total</p>
-                  <p>Rp. 3,430,000</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="w-4/5 sm:w-full flex flex-col items-center bg-white text-headline p-2 rounded-md mb-4">
-              <div className="w-full flex text-center text-xs sm:text-sm md:text-base lg:text-lg py-3 border-b-2">
-                <div className="flex-1">This month</div>
-              </div>
-              <div className="w-full sm:py-6 sm:px-12 md:px-16 flex flex-col text-xs sm:text-sm md:text-base p-3 justify-between gap-2 sm:gap-6">
-                {nodes?.map((transaction, idx) => {
-                  return (
-                    <ThisMonthTransactionsItem
-                      key={idx}
-                      name={transaction.name}
-                      amount={transaction.amount}
-                      onClick={() => onTransactionClick(transaction.id)}
-                      onDelete={() => onDeleteHandler(transaction.id)}
-                    />
-                  )
-                })}
-              </div>
+            <div className="w-full sm:py-6 sm:px-12 md:px-16 flex flex-col text-xs sm:text-sm md:text-base p-3 justify-between gap-2 sm:gap-6">
+              {nodes?.map((transaction, idx) => {
+                return (
+                  <ThisMonthTransactionsItem
+                    key={idx}
+                    name={transaction.name}
+                    amount={transaction.amount}
+                    onClick={() => onTransactionClick(transaction.id)}
+                    onDelete={() => onDeleteHandler(transaction.id)}
+                  />
+                )
+              })}
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </div>
   )
 }
