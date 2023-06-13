2de627fa5873685cc7c8ae896d8e54e3f1facd9c
Maulana Dimyati <dimyatimaulana@gmail.com>
2023-06-13 16:49:26 +0700

    feat: add testimonials section

--- src/page/landing_page/LandingPage.jsx
+++ src/page/landing_page/LandingPage.jsx
@@ -1,5 +1,15 @@
 import { Button } from '../../components/button/Button'
 import { useNavigate } from 'react-router-dom'
+import { Icon } from '@iconify/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import './LandingPage.css'
+
+import { Autoplay, Pagination, Navigation } from 'swiper'
 
 export const LandingPage = () => {
   const navigate = useNavigate()
@@ -112,6 +122,172 @@ export const LandingPage = () => {
         </div>
       </section>
 
+      <section className="bg-white">
+        <div className="pt-24 px-20px sm:px-40px md:px-50px">
+          <h2 className="text-3xl font-bold text-headline text-center md:text-4xl">
+            Testimonials
+          </h2>
+        </div>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 20000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <figure className="bg-white border-2 border-dashed border-primary rounded-xl drop-shadow-lg overflow-hidden ">
+              <blockquote className="p-4 sm:p-8">
+                <Icon
+                  className="text-primary"
+                  icon="icon-park-outline:quote"
+                />
+                <p className="text-base px-10px">
+                  Mantap!! UI/UX nya bagus mudah dimengerti oleh pengguna!
+                </p>
+              </blockquote>
+              <div className="w-full flex items-center justify-between px-8 py-4 bg-primary">
+                <div className="flex items-center gap-5">
+                  <div className="rounded-full border-2 w-10 h-10 border-white">
+                    <img src="/svgs/avatar.svg" />
+                  </div>
+                  <figcaption className="text-white text-sm text-left">
+                    <div>Maulana Dimyati</div>
+                    <div className="opacity-70">
+                      Student at Dicoding Academy
+                    </div>
+                  </figcaption>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <figure className="bg-white border-2 border-dashed border-primary rounded-xl drop-shadow-lg overflow-hidden">
+              <blockquote className="p-4 sm:p-8">
+                <Icon
+                  className="text-primary"
+                  icon="icon-park-outline:quote"
+                />
+                <p className="text-base px-10px">
+                  Moneta telah membantu saya melihat alur keuangan setiap bulan
+                </p>
+              </blockquote>
+              <div className="w-full flex items-center justify-between px-8 py-4 bg-primary">
+                <div className="flex items-center gap-5">
+                  <div className="rounded-full border-2 w-10 h-10 border-white">
+                    <img src="/svgs/avatar.svg" />
+                  </div>
+                  <figcaption className="text-white text-sm text-left">
+                    <div>David Yapetter</div>
+                    <div className="opacity-70">
+                      Student at Dicoding Academy
+                    </div>
+                  </figcaption>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <figure className="bg-white border-2 border-dashed border-primary rounded-xl drop-shadow-lg overflow-hidden">
+              <blockquote className="p-4 sm:p-8">
+                <Icon
+                  className="text-primary"
+                  icon="icon-park-outline:quote"
+                />
+                <p className="text-base px-10px">
+                  Mudah digunakan.. suka banget sama fitur - fiturnya 🥰
+                </p>
+              </blockquote>
+              <div className="w-full flex items-center justify-between px-8 py-4 bg-primary">
+                <div className="flex items-center gap-5">
+                  <div className="rounded-full border-2 w-10 h-10 border-white">
+                    <img src="/svgs/avatar.svg" />
+                  </div>
+                  <figcaption className="text-white text-sm text-left">
+                    <div>Annisa Syasqia Zain</div>
+                    <div className="opacity-70">
+                      Student at Dicoding Academy
+                    </div>
+                  </figcaption>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <figure className="bg-white border-2 border-dashed border-primary rounded-xl drop-shadow-lg overflow-hidden">
+              <blockquote className="p-4 sm:p-8">
+                <Icon
+                  className="text-primary"
+                  icon="icon-park-outline:quote"
+                />
+                <p className="text-base px-10px">
+                  Good Job! Walaupun sudah bagus jangan berpuas hati, ditunggu
+                  pengembangan kedepannya.
+                </p>
+              </blockquote>
+              <div className="w-full flex items-center justify-between px-8 py-4 bg-primary">
+                <div className="flex items-center gap-5">
+                  <div className="rounded-full border-2 w-10 h-10 border-white">
+                    <img src="/svgs/avatar.svg" />
+                  </div>
+                  <figcaption className="text-white text-sm text-left">
+                    <div>Fioreza Radhin Naufal</div>
+                    <div className="opacity-70">
+                      Student at Dicoding Academy
+                    </div>
+                  </figcaption>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <figure className="bg-white border-2 border-dashed border-primary rounded-xl drop-shadow-lg overflow-hidden">
+              <blockquote className="p-4 sm:p-8">
+                <Icon
+                  className="text-primary"
+                  icon="icon-park-outline:quote"
+                />
+                <p className="text-base px-10px">
+                  Keren nih semua fungsional sudah dapat dijalankan. Desainnya
+                  juga cukup baik
+                </p>
+              </blockquote>
+              <div className="w-full flex items-center justify-between px-8 py-4 bg-primary">
+                <div className="flex items-center gap-5">
+                  <div className="rounded-full border-2 w-10 h-10 border-white">
+                    <img src="/svgs/avatar.svg" />
+                  </div>
+                  <figcaption className="text-white text-sm text-left">
+                    <div>Egy Yuda Nugraha</div>
+                    <div className="opacity-70">
+                      Student at Dicoding Academy
+                    </div>
+                  </figcaption>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+        </Swiper>
+      </section>
       <section className="bg-white">
         <div className="py-24 text-center px-20px sm:px-40px md:px-50px">
           <h2 className="mb-4 text-4xl font-bold text-headline md:text-5xl">
