1b7c0d66f9be31564f742f86780d737562b392e5
Annisa Syasqia Zain <annisasyasqia.20063@mhs.unesa.ac.id>
2023-06-15 10:25:49 +0700

    fix: forgot to add translation of login history page title

--- src/page/login_history/LoginHistory.jsx
+++ src/page/login_history/LoginHistory.jsx
@@ -2,8 +2,10 @@ import { useQuery } from 'react-query'
 import { ReactQueryKeys } from '../../api/constant'
 import { AuthApi } from '../../api/auth/authApi'
 import LoginHistoryItem from './LoginHistoryItem'
+import { useIntl } from 'react-intl'
 
 export const LoginHistoryPage = () => {
+  const intl = useIntl()
   const { data: loginHistories, isLoading } = useQuery(
     ReactQueryKeys.LOGIN_HISTORIES,
     () => AuthApi.loginHistories().then((r) => r.data?.login_histories),
@@ -20,7 +22,9 @@ export const LoginHistoryPage = () => {
       ) : (
         //<h4>{JSON.stringify(loginHistories)}</h4>
         <div className="flex flex-col py-40px px-20px sm:px-100px lg:px-200px">
-          <h4 className="font-bold text-headline text-2xl">Login History</h4>
+          <h4 className="font-bold text-headline text-2xl">
+            {intl.formatMessage({ id: 'loginHistory' })}
+          </h4>
           <div className="mt-8">
             {loginHistories.map((loginHistory) => (
               <LoginHistoryItem
